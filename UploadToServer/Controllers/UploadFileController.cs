d97d12222fb42f38c5f2ceef74ab8a84b0841107
razanwael1122 <razanwael1122@gmail.com>
2023-06-20 11:17:34 +0300

    Add project files.

---
+++ UploadToServer/Controllers/UploadFileController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace UploadToServer.Controllers
+{
+    [ApiController]// use the model binding feature of ASP.NET Core to bind
+                   // the incoming HTTP request data to the action method parameters.
+    [Route("[controller]")]
+    public class UploadFileController : Controller
+    {//The ILogger is used for logging purposes,
+     //and IWebHostEnvironment provides information about the web hosting environment.
+        private readonly ILogger<UploadFileController> _logger;
+        private readonly IWebHostEnvironment _environment;
+        public UploadFileController (ILogger<UploadFileController>logger,IWebHostEnvironment environment)
+        {
+            _logger = logger; 
+            _environment= environment ?? throw new ArgumentNullException(nameof(environment));
+          
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            try
+            {
+                var httpRequest = HttpContext.Request;
+                if (httpRequest.Form.Files.Count>0) 
+                { 
+                    foreach(var file in httpRequest.Form.Files)
+                    {
+                        var filePath = Path.Combine(_environment.ContentRootPath, "Uploads");
+
+                        if(!Directory.Exists(filePath))
+                            Directory.CreateDirectory(filePath);
+
+                        using (var memoryStream=new MemoryStream())
+                        {
+                            await file.CopyToAsync(memoryStream);
+                            System.IO.File.WriteAllBytes(Path.Combine(filePath,file.FileName),memoryStream.ToArray());
+                        }
+                        return Ok();
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                _logger.LogError(e, "Error");
+                return new StatusCodeResult(500);
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ task6/task6/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace task6
+{
+    public partial class MainPage : ContentPage
+    {
+     
+        public MainPage()
+        {
+            InitializeComponent();
+            {
+                
+            }
+        }
+
+        async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            
+            var file = await MediaPicker.PickPhotoAsync();
+            if (file == null)
+                return;
+            var content = new MultipartFormDataContent();
+            content.Add(new StreamContent(await file.OpenReadAsync()), "file", file.FileName);
+
+            var httpClient=new HttpClient();
+            var response = await httpClient.PostAsync("http://192.168.248.1:7252/UploadFile", content);
+       
+
+
+            StatusLabel.Text=response.StatusCode.ToString();
+            
+        }
+
+    }
+}
+
+
+
+
+
